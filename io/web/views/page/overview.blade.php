a05fb006d0bda99030ebdab752d5131dccef703c
Aron Wouters <lionar@mail.com>
2019-09-30 15:40:35 +0200

    Corrected active links

--- io/web/views/page/overview.blade.php
+++ io/web/views/page/overview.blade.php
@@ -38,10 +38,10 @@
                         <div class="mdc-tab-scroller__scroll-area">
                             <div class="mdc-tab-scroller__scroll-content">
                                 
-                                <a href="/exercises" class="mdc-tab mdc-tab--stacked {{  ( request::getUri ( )->getPath ( ) === '/exercises' ) ? 'mdc-tab--active' : '' }}" role="tab" aria-selected="true" tabindex="0">
+                                <a href="/exercises" class="mdc-tab mdc-tab--stacked {{  ( substr ( request::getUri ( )->getPath ( ), 0, 10 ) === '/exercises' ) ? 'mdc-tab--active' : '' }}" role="tab" aria-selected="true" tabindex="0">
                                     <span class="mdc-tab__content">
                                         <span class="mdc-tab__icon" aria-hidden="true">
-                                            @if ( request::getUri ( )->getPath ( ) === '/exercises' )
+                                            @if ( substr ( request::getUri ( )->getPath ( ), 0, 10 ) === '/exercises' )
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                                     <path d="M19 3H4.99c-1.11 0-1.98.89-1.98 2L3 19c0 1.1.88 2 1.99 2H19c1.1 0 2-.9 2-2V5c0-1.11-.9-2-2-2zm0 12h-4c0 1.66-1.35 3-3 3s-3-1.34-3-3H4.99V5H19v10z"/>
                                                     <path fill="none" d="M0 0h24v24H0V0z"/>
@@ -55,16 +55,16 @@
                                         </span>
                                         <span class="mdc-tab__text-label">Exercises</span>
                                     </span>
-                                    <span class="mdc-tab-indicator {{ ( request::getUri ( )->getPath ( ) === '/' ) ? 'mdc-tab-indicator--active' : '' }}">
+                                    <span class="mdc-tab-indicator {{ ( substr ( request::getUri ( )->getPath ( ), 0, 10 ) === '/exercises' ) ? 'mdc-tab-indicator--active' : '' }}">
                                         <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
                                     </span>
                                     <span class="mdc-tab__ripple"></span>
                                 </a>
 
-                                <a href="/schemes" class="mdc-tab mdc-tab--stacked {{ ( request::getUri ( )->getPath ( ) === '/schemes' ) ? 'mdc-tab--active' : '' }}" role="tab" aria-selected="true" tabindex="0">
+                                <a href="/schemes" class="mdc-tab mdc-tab--stacked {{ ( substr ( request::getUri ( )->getPath ( ), 0, 8 ) === '/schemes' ) ? 'mdc-tab--active' : '' }}" role="tab" aria-selected="true" tabindex="0">
                                     <span class="mdc-tab__content">
                                         <span class="mdc-tab__icon" aria-hidden="true">
-                                            @if (  request::getUri ( )->getPath ( ) === '/schemes' )
+                                            @if ( substr ( request::getUri ( )->getPath ( ), 0, 8 ) === '/schemes' )
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                                     <path d="M0 0h24v24H0z" fill="none"/>
                                                     <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm2 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>
@@ -78,7 +78,7 @@
                                         </span>
                                         <span class="mdc-tab__text-label">Schemes</span>
                                     </span>
-                                    <span class="mdc-tab-indicator {{ ( request::getUri ( )->getPath ( ) === '/schemes' ) ? 'mdc-tab-indicator--active' : '' }}">
+                                    <span class="mdc-tab-indicator {{ ( substr ( request::getUri ( )->getPath ( ), 0, 8 ) === '/schemes' ) ? 'mdc-tab-indicator--active' : '' }}">
                                         <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
                                     </span>
                                     <span class="mdc-tab__ripple"></span>
--- io/web/views/partials/drawer.blade.php
+++ io/web/views/partials/drawer.blade.php
@@ -1,8 +1,8 @@
 <aside class="mdc-drawer mdc-elevation--z2">
     <div class="mdc-drawer__content">
-        <nav class="mdc-list">
-            <a class="mdc-list-item {{ ( request::getUri ( )->getPath ( ) === '/exercises' ) ? 'mdc-list-item--activated' : '' }}" href="/exercises">
-                @if ( request::getUri ( )->getPath ( ) === '/exercises' )
+        <nav class="mdc-list">            
+            <a class="mdc-list-item {{ ( substr ( request::getUri ( )->getPath ( ), 0, 10 ) === '/exercises' ) ? 'mdc-list-item--activated' : '' }}" href="/exercises">
+                @if ( substr ( request::getUri ( )->getPath ( ), 0, 10 ) === '/exercises' )
                     <svg class="mdc-list-item__graphic" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M19 3H4.99c-1.11 0-1.98.89-1.98 2L3 19c0 1.1.88 2 1.99 2H19c1.1 0 2-.9 2-2V5c0-1.11-.9-2-2-2zm0 12h-4c0 1.66-1.35 3-3 3s-3-1.34-3-3H4.99V5H19v10z"/>
                         <path fill="none" d="M0 0h24v24H0V0z"/>
@@ -15,8 +15,8 @@
                 @endif
                 <span class="mdc-list-item__text">Exercises</span>
             </a>
-            <a class="mdc-list-item {{ ( request::getUri ( )->getPath ( ) === '/schemes' ) ? 'mdc-list-item--activated' : '' }}" href="/schemes">
-                @if (  request::getUri ( )->getPath ( ) === '/schemes' )
+            <a class="mdc-list-item {{ ( substr ( request::getUri ( )->getPath ( ), 0, 8 ) === '/schemes' ) ? 'mdc-list-item--activated' : '' }}" href="/schemes">
+                @if ( substr ( request::getUri ( )->getPath ( ), 0, 8 ) === '/schemes' )
                     <svg class="mdc-list-item__graphic" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M0 0h24v24H0z" fill="none"/>
                         <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm2 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>
