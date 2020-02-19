7cd2b31116f16d82628c55b9e93aba23f8097cbc
Aron Wouters <lionar@mail.com>
2020-02-19 11:40:38 +0100

    Added person to redirect

--- io/web/statuses/1001. session started.php
+++ io/web/statuses/1001. session started.php
@@ -4,5 +4,6 @@ use function compact as with;
 
 status::matching ( 1001, function ( session $session )
 {
-    return redirect::to ( '/session/' . $session->id );
+    $person = app::make ( person::class );
+    return redirect::to ( '/session/' . $session->id . '?person=' . $person->name );
 } );
