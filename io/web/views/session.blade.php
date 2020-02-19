25a983e94162a553c7450cfc8959060fd4163dc4
Aron Wouters <lionar@mail.com>
2020-02-19 15:00:30 +0100

    Using data table for sessions

--- io/web/views/session.blade.php
+++ io/web/views/session.blade.php
@@ -9,23 +9,43 @@
 @endsection
 
 @section ( 'page' )
-    <form action="/session/{{ $session->id }}" method="POST">
-    
-        @foreach ( $scheme->exercises as $exercise )
-            <h2 class="mdc-typography--subtitle1">{{ $exercise->name }}</h2>
-            @for ( $i = 1; $i <= $exercise->sets; $i++ )
-                <div class="mdc-text-field">
-                    <input 
-                        type="text" 
-                        id="{{ $exercise->id }}-{{ $i }}" 
-                        class="mdc-text-field__input" 
-                        name="exercises[{{ $exercise->id }}][{{ $i }}]" 
-                        value="{{ $session->getCompletion ( $exercise->id, $i ) ?? '' }}">
-                    <label for="{{ $exercise->id }}-{{ $i }}" class="mdc-floating-label">Set {{ $i }}</label>
-                    <div class="mdc-line-ripple"></div>
-                </div>
-            @endfor
-        @endforeach
+    <form action="/session/{{ $session->id }}" method="POST" style="display: grid;">
+        <div class="mdc-data-table" style="margin: 0 auto; max-width: 100%;">
+            <table class="mdc-data-table__table" aria-label="Dessert calories">
+                <thead>
+                    <tr class="mdc-data-table__header-row">
+                        <th class="mdc-data-table__header-cell" role="columnheader" scope="col">Exercise</th>
+                        <th class="mdc-data-table__header-cell mdc-data-table__header-cell--numeric" role="columnheader" scope="col">Set 1</th>
+                        <th class="mdc-data-table__header-cell mdc-data-table__header-cell--numeric" role="columnheader" scope="col">Set 2</th>
+                        <th class="mdc-data-table__header-cell mdc-data-table__header-cell--numeric" role="columnheader" scope="col">Set 3</th>
+                        <th class="mdc-data-table__header-cell mdc-data-table__header-cell--numeric" role="columnheader" scope="col">Set 4</th>
+                    </tr>
+                </thead>
+                <tbody class="mdc-data-table__content">
+                    @foreach ( $scheme->exercises as $exercise )
+                        <tr class="mdc-data-table__row">
+                            <td class="mdc-data-table__cell">{{ $exercise->name }}</td>
+
+                            @for ( $i = 1; $i <= $exercise->sets; $i++ )
+                                <td class="mdc-data-table__cell mdc-data-table__cell--numeric">
+                                    <input
+                                        step="0.5"
+                                        type="number" 
+                                        id="{{ $exercise->id }}-{{ $i }}" 
+                                        name="exercises[{{ $exercise->id }}][{{ $i }}]" 
+                                        value="{{ $session->getCompletion ( $exercise->id, $i ) ?? '0' }}">
+                                </td>
+                            @endfor
+                            @if ( $exercise->sets === 3 )
+                                <td class="mdc-data-table__cell mdc-data-table__cell--numeric" style="color: rgba(0,0,0,.6);">
+                                    n/a
+                                </td>
+                            @endif
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 
         @include ( 'partials.form.fab', [ 'action' => 'save' ] )
     </form>
@@ -36,8 +56,6 @@
     @parent
     
     <script>
-        document.querySelectorAll ( '.mdc-text-field' ).forEach( ( input ) => {
-            mdc.textField.MDCTextField.attachTo ( input );
-        });
+        mdc.dataTable.MDCDataTable.attachTo ( document.querySelector ( '.mdc-data-table' ) );
     </script>    
 @endsection
