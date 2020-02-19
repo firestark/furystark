ddb4adbdcaaa0de5deec565805f8ff56ac9c58e0
Aron Wouters <lionar@mail.com>
2020-02-19 12:44:31 +0100

    Removed active person as switch option

--- io/web/views/partials/account.blade.php
+++ io/web/views/partials/account.blade.php
@@ -5,16 +5,19 @@
 
     <div class="mdc-menu mdc-menu-surface" id="avatar-menu">
         <ul class="mdc-list mdc-list--linked" role="menu" aria-hidden="true" aria-orientation="vertical" tabindex="-1" style="min-width: 240px;">
-            <a href="/person/Martijn">
-                <li class="mdc-list-item" role="menuitem">
-                    <span class="mdc-list-item__text">Martijn</span>
-                </li>
-            </a>
-            <a href="/person/Aron">
-                <li class="mdc-list-item" role="menuitem">
-                    <span class="mdc-list-item__text">Aron</span>
-                </li>
-            </a>
+            @if ( app::make ( person::class )->name === 'Aron' )
+                <a href="/person/Martijn">
+                    <li class="mdc-list-item" role="menuitem">
+                        <span class="mdc-list-item__text">Martijn</span>
+                    </li>
+                </a>
+            @else
+                <a href="/person/Aron">
+                    <li class="mdc-list-item" role="menuitem">
+                        <span class="mdc-list-item__text">Aron</span>
+                    </li>
+                </a>
+            @endif
         </ul>
     </div>
 </div>
