35c36a0e78cbe3b001ec2bc6a6e7c7b710b8534d
Aron Wouters <lionar@mail.com>
2020-03-05 14:59:36 +0100

    Added exercises view for scheme

--- io/web/views/schemes/exercises.blade.php
+++ io/web/views/schemes/exercises.blade.php
@@ -38,5 +38,69 @@
 @endsection
 
 @section('page')
-    <h1>Scheme exercises</h1>
+    <ul class="mdc-list mdc-list--two-line">
+        @foreach ($scheme->exercises as $exercise)
+            <li class="mdc-list-item" tabindex="0">
+                {{-- <a href="/schemes/{{ $scheme->id }}/start" class="mdc-list-item__graphic" >
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                        <path d="M8 5v14l11-7z"/>
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                    </svg>
+                </a> --}}
+                <span class="mdc-list-item__text" style="width: 100%;">
+                    <a href="#">
+                        <span style="width: 100%;" class="mdc-list-item__primary-text">{{ $exercise->name }}</span>
+                        <span style="width: 100%;" class="mdc-list-item__secondary-text">{{ $exercise->sets }}x{{ $exercise->reps }}</span>
+                    </a>
+                </span>
+                <a href="/schemes/{{ $scheme->id }}/remove/?" class="mdc-list-item__meta" aria-hidden="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                    </svg>
+                </a>
+            </li>
+        @endforeach
+    </ul>
+
+
+    {{-- <form action="/schemes" method="POST" style="padding: 16px;">
+        <label class="mdc-text-field" id="title-input">
+            <div class="mdc-text-field__ripple"></div>
+            <input name="title" class="mdc-text-field__input" type="text" value="{{ $scheme->name }}">
+            <span class="mdc-floating-label" id="my-label-id">Title</span>
+            <div class="mdc-line-ripple"></div>
+        </label>
+
+        <h2 class="mdc-typography--headline6">Exercises</h2>
+
+        
+
+        @for ($i = 0; $i < 8; $i++)
+            <div style="margin-bottom: 32px;">
+                <label class="mdc-text-field" id="exercise-input">
+                    <div class="mdc-text-field__ripple"></div>
+                    <input name="exercise[{{ $i }}][name]" class="mdc-text-field__input" type="text">
+                    <span class="mdc-floating-label" id="my-label-id">Exercise {{ $i +1 }}</span>
+                    <div class="mdc-line-ripple"></div>
+                </label>
+
+                <label class="mdc-text-field" id="sets-input">
+                    <div class="mdc-text-field__ripple"></div>
+                        <input name="exercise[{{ $i }}][sets]" class="mdc-text-field__input" type="number">
+                        <span class="mdc-floating-label" id="my-label-id">Sets</span>
+                    <div class="mdc-line-ripple"></div>
+                </label>
+
+                <label class="mdc-text-field" id="reps-input">
+                    <div class="mdc-text-field__ripple"></div>
+                        <input name="exercise[{{ $i }}][reps]" class="mdc-text-field__input" type="number">
+                        <span class="mdc-floating-label" id="my-label-id">Reps</span>
+                    <div class="mdc-line-ripple"></div>
+                </label>
+            </div>
+        @endfor
+
+        @include('partials.form.fab', ['action' => 'save'])
+    </form> --}}
 @endsection
