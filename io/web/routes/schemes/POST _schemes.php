88ce98d8b1f6b1e62bfe04e1be8515b13aae7482
Aron Wouters <lionar@mail.com>
2020-03-13 13:28:40 +0100

    Bugfix

--- io/web/routes/schemes/POST _schemes.php
+++ io/web/routes/schemes/POST _schemes.php
@@ -1,7 +1,5 @@
 <?php
 
-use function compact as with;
-
 Route::post('/schemes', function() {
-    return App::fulfill('i want to add a scheme', with('scheme'));
+    return App::fulfill('i want to add a scheme');
 });
