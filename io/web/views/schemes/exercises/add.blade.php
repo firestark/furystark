958b8ed6934389f1f2480f3dbb04ef33f7ce6edd
Aron Wouters <ninelionar@gmail.com>
2020-05-14 14:13:56 +0200

    Add back arrow and fab

--- io/web/views/schemes/exercises/add.blade.php
+++ io/web/views/schemes/exercises/add.blade.php
@@ -1,43 +1,36 @@
 @extends('page.main')
 
+@section('navigation')
+    @include('partials.up-arrow', ['link' => '/schemes/' . $schemeId . '/exercises'])
+@endsection
+
 @section('title')
-    Add an exercise
+    Add exercise
 @endsection
 
 @section('page')
-    <div class="mdc-card">
+    <div class="mdc-card" style="padding: 16px; position: static;">
         <form action="/schemes/{{ $schemeId }}/exercises" method="POST">
-            <div class="mdc-dialog__content" id="my-dialog-content">
-                <label class="mdc-text-field" id="exercise-input">
-                    <div class="mdc-text-field__ripple"></div>
-                    <input name="exercise" class="mdc-text-field__input" type="text" required>
-                    <span class="mdc-floating-label" id="my-label-id">Exercise</span>
-                    <div class="mdc-line-ripple"></div>
-                </label>
-                <label class="mdc-text-field" id="sets-input">
-                    <div class="mdc-text-field__ripple"></div>
-                        <input name="sets" class="mdc-text-field__input" type="number" required>
-                        <span class="mdc-floating-label" id="my-label-id">Sets</span>
-                    <div class="mdc-line-ripple"></div>
-                </label>
+            <label class="mdc-text-field" id="exercise-input">
+                <div class="mdc-text-field__ripple"></div>
+                <input name="exercise" class="mdc-text-field__input" type="text" required>
+                <span class="mdc-floating-label" id="my-label-id">Exercise</span>
+                <div class="mdc-line-ripple"></div>
+            </label>
+            <label class="mdc-text-field" id="sets-input">
+                <div class="mdc-text-field__ripple"></div>
+                    <input name="sets" class="mdc-text-field__input" type="number" required>
+                    <span class="mdc-floating-label" id="my-label-id">Sets</span>
+                <div class="mdc-line-ripple"></div>
+            </label>
 
-                <label class="mdc-text-field" id="reps-input">
-                    <div class="mdc-text-field__ripple"></div>
-                        <input name="reps" class="mdc-text-field__input" type="number" required>
-                        <span class="mdc-floating-label" id="my-label-id">Reps</span>
-                    <div class="mdc-line-ripple"></div>
-                </label>
-            </div>
-            <footer>
-                <button type="button" class="mdc-button mdc-dialog__button" data-mdc-dialog-action="no">
-                    <div class="mdc-button__ripple"></div>
-                    <span class="mdc-button__label">Cancel</span>
-                </button>
-                <button type="submit" class="mdc-button mdc-dialog__button">
-                    <div class="mdc-button__ripple"></div>
-                    <span class="mdc-button__label">Add</span>
-                </button>
-            </footer>
+            <label class="mdc-text-field" id="reps-input">
+                <div class="mdc-text-field__ripple"></div>
+                    <input name="reps" class="mdc-text-field__input" type="number" required>
+                    <span class="mdc-floating-label" id="my-label-id">Reps</span>
+                <div class="mdc-line-ripple"></div>
+            </label>
+            @include('partials.form.fab', ['action' => 'save'])
         </form>
     </div>
 @endsection
