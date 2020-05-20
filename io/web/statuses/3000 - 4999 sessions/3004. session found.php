b4abf0a535a7f95cc81ada0de8ae180aa9587f51
Aron Wouters <ninelionar@gmail.com>
2020-05-20 21:10:05 +0200

    Corrected scheme id name

--- io/web/statuses/3000 - 4999 sessions/3004. session found.php
+++ io/web/statuses/3000 - 4999 sessions/3004. session found.php
@@ -3,6 +3,6 @@
 use function compact as with;
 
 Status::matching(3004, function(Session $session) {
-    $scheme = App::make(Scheme::class, ['id' => $session->scheme]);
+    $scheme = App::make(Scheme::class, ['schemeId' => $session->scheme]);
     return View::make('session', with('session', 'scheme'));
 });
